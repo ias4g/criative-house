52f3d3e2bd5100713f5cfe2b1c92b00a7f63f055
Ias4g <izaell.universitario@outlook.com>
2020-03-30 16:21:36 -0300

    Configuring the express server to access static files, styles, scripts and images.

---
+++ server.js
@@ -0,0 +1,22 @@
+// Usando o EXPRESS para criar e configurar o servidor
+const express = require('express')
+const server = express()
+
+// Configurando arquivos estáticos como styles, scripts e images
+server.use(express.static("public"))
+
+// Configurando o NUNJUCKS
+const nunjucks = require('nunjucks')
+
+
+// Criando uma rota raiz '/'
+server.get("/", function(req, res){
+    return res.sendFile(__dirname + '/index.html')
+})
+
+server.get("/ideias", function(req, res){
+    return res.sendFile(__dirname + '/ideias.html')
+})
+
+// Colocando server para rodar na porta 3000
+server.listen(3000)
