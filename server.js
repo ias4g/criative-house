4624ec2b3a24fccc5de87cb86010072f6b940f4f
Ias4g <izaell.universitario@outlook.com>
2020-04-04 19:10:05 -0300

    Instalando e configurando o bd sqlite3, e corrigindo os erros existentes

--- server.js
+++ server.js
@@ -2,56 +2,58 @@
 const express = require('express')
 const server = express()
 
-const ideas = [
+const db = require('./db')
 
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-        title: "Cursos de programação",
-        category: "Estudos",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
-        url: "https://www.github.com/Ias4g"
-    },
+// const ideas = [
 
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
-        title: "Exercícios",
-        category: "Saúde",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
-        url: "https://www.github.com/Ias4g"
-    },
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+//         title: "Cursos de programação",
+//         category: "Estudos",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+//         url: "https://www.github.com/Ias4g"
+//     },
 
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
-        title: "Meditação",
-        category: "Mentalidade",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
-        url: "https://www.github.com/Ias4g"
-    },
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+//         title: "Exercícios",
+//         category: "Saúde",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+//         url: "https://www.github.com/Ias4g"
+//     },
 
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
-        title: "Karaokê",
-        category: "Diversão em familia",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
-        url: "https://www.github.com/Ias4g"
-    },
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+//         title: "Meditação",
+//         category: "Mentalidade",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+//         url: "https://www.github.com/Ias4g"
+//     },
 
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729038.svg",
-        title: "Pinturas",
-        category: "Criatividades",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
-        url: "https://www.github.com/Ias4g"
-    },
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+//         title: "Karaokê",
+//         category: "Diversão em familia",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+//         url: "https://www.github.com/Ias4g"
+//     },
 
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729048.svg",
-        title: "Recortes",
-        category: "Criatividades",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
-        url: "https://www.github.com/Ias4g"
-    }
-]
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729038.svg",
+//         title: "Pinturas",
+//         category: "Criatividades",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+//         url: "https://www.github.com/Ias4g"
+//     },
+
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729048.svg",
+//         title: "Recortes",
+//         category: "Criatividades",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+//         url: "https://www.github.com/Ias4g"
+//     }
+// ]
 
 // Configurando arquivos estáticos como styles, scripts e images
 server.use(express.static("public"))
@@ -66,22 +68,28 @@ const nunjucks = require('nunjucks')
 
 // Criando uma rota raiz '/'
 server.get("/", function(req, res){
+    db.all(`SELECT* FROM ideas`, function(err, rows){
+        if (err) return console.log(err)
 
-    const reversedIdeas = [...ideas].reverse()
+        const reversedIdeas = [...rows].reverse()
 
-    let lastIdeas = []
-    for(let idea of reversedIdeas){
-        if(lastIdeas.length < 2){
-            lastIdeas.push(idea)
+        let lastIdeas = []
+        for(let idea of reversedIdeas){
+            if(lastIdeas.length < 2){
+                lastIdeas.push(idea)
+            }
         }
-    }
 
-    return res.render('index.html', { ideas: lastIdeas })
+        return res.render('index.html', { ideas: lastIdeas })
+    })
 })
 
 server.get("/ideias", function(req, res){
-    const reversedIdeas = [...ideas].reverse()
-    return res.render('ideias.html', { ideas: reversedIdeas })
+    db.all(`SELECT* FROM ideas`, function(err, rows){
+        if (err) return console.log(err)
+        const reversedIdeas = [...rows].reverse()
+        return res.render('ideias.html', { ideas: reversedIdeas })
+    })
 })
 
 // Colocando server para rodar na porta 3000
