d7d22c44c1317c58263b378378af885f7c6b6b05
Ias4g <izaell.universitario@outlook.com>
2020-04-07 15:33:01 -0300

    Reestruturanso o CSS das pages, e ingnorando a pasta de banco de dados

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database('./creative-house.db')
+const db = new sqlite3.Database('./db/creative-house.db')
  
 db.serialize(function(){
 
